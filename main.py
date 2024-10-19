52fd87f61dd85d817c26a0477a6ec92e4a785499
Alexrazumovich <alexrazumovich@gmail.com>
2024-10-19 14:16:31 +0300

    weather_news-quote

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 def index():
     weather=None
     news=None
+    quote=None
     if request.method == 'POST':
         city = request.form.get('city')
         weather = get_weather(city)
         news=get_news()
-    return render_template('index.html',weather=weather,news=news)
+        quote=get_quote()
+
+    return render_template('index.html',weather=weather,news=news,quote=quote)
 def get_weather(city):
         api_key = '83ae226b645257424cfc78b3aa9e1aed'
         url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
@@ -23,6 +26,12 @@ def get_news():
     url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}'
     response = requests.get(url)
     return response.json().get('articles',[])
+
+def get_quote():
+    # url = f'https://api.quotable.io/random'
+    url=f'https://quoteapi.pythonanywhere.com/random'
+    response = requests.get(url)
+    return response.json()
 
 if __name__ == '__main__':
     app.run(debug=True)
